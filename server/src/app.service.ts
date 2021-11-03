924a89b682c0c58000734ff7900188475034533b
Clemens Horn <clemens.horn@mni.thm.de>
2021-11-03 23:10:33 +0100

    add coverage reports and build artifacts to pipeline
    Signed-off-by: Clemens Horn <clemens.horn@mni.thm.de>

--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -1,7 +1,15 @@
 import { Injectable } from '@nestjs/common';
 
+/**
+ * App Service
+ */
 @Injectable()
 export class AppService {
+
+    /**
+    * Function that returns Hello World
+    * @returns Hello World string
+    */
     getHello(): string {
         return 'Hello World!';
     }
