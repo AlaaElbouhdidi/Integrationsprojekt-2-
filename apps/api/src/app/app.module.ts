40f250d5cc9e27b941d545ba4ebc4b52fbf259b4
Clemens Horn <clemens.horn@mni.thm.de>
2021-11-08 15:29:13 +0100

    initalize firebase admin without parameters, load .env with configmodule
    Signed-off-by: Clemens Horn <clemens.horn@mni.thm.de>

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -2,17 +2,17 @@ import { Module } from '@nestjs/common';
 import { AppController } from './controllers/app/app.controller';
 import { AppService } from './services/app/app.service';
 import { FirebaseModule } from './modules/firebase.module';
-// import { ConfigModule } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './modules/user.module';
 
 @Module({
     imports: [
         FirebaseModule,
         UserModule,
-        // ConfigModule.forRoot({
-        //     isGlobal: true,
-        //     envFilePath: './.env',
-        // }),
+        ConfigModule.forRoot({
+            isGlobal: true,
+            envFilePath: './.env',
+        }),
     ],
     controllers: [AppController],
     providers: [AppService],
