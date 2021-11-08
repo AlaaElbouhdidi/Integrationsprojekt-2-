ce9e0f467a63ce0dcf094563e42300d230b37ce8
Clemens Horn <clemens.horn@mni.thm.de>
2021-11-08 12:49:37 +0100

    update dependencies in function
    Signed-off-by: Clemens Horn <clemens.horn@mni.thm.de>

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -2,18 +2,17 @@ import { Module } from '@nestjs/common';
 import { AppController } from './controllers/app/app.controller';
 import { AppService } from './services/app/app.service';
 import { FirebaseModule } from './modules/firebase.module';
-import { ConfigModule } from '@nestjs/config';
+// import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './modules/user.module';
 
 @Module({
     imports: [
         FirebaseModule,
-        ConfigModule.forRoot({
-            isGlobal: true,
-            envFilePath: './.env',
-        }),
         UserModule,
-       
+        // ConfigModule.forRoot({
+        //     isGlobal: true,
+        //     envFilePath: './.env',
+        // }),
     ],
     controllers: [AppController],
     providers: [AppService],
