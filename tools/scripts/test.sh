39051dfa41676789b8eeca73c20fbca9c6eca81e
Clemens Horn <clemens.horn@mni.thm.de>
2021-11-10 21:56:06 +0100

    retry pipeline
    Signed-off-by: Clemens Horn <clemens.horn@mni.thm.de>

--- tools/scripts/test.sh
+++ tools/scripts/test.sh
@@ -6,4 +6,4 @@ nx run-many --target=lint --all=true --skip-nx-cache &&
 nx run-many --target=test --all=true --skip-nx-cache &&
 nx run-many --target=compodoc --all=true --skip-nx-cache &&
 nx run-many --target=e2e --all=true --skip-nx-cache &&
-echo "Looking good, pipeline is likely going to succeed! :)"
+echo "Looking good, pipeline is likely going to succeed!"
