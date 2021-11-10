f1fa194e082546c6c3362d1f586a6075546390ee
Clemens Horn <clemens.horn@mni.thm.de>
2021-11-10 05:15:37 +0100

    import header in spec file
    Signed-off-by: Clemens Horn <clemens.horn@mni.thm.de>

--- apps/mate-team/src/app/app.component.spec.ts
+++ apps/mate-team/src/app/app.component.spec.ts
@@ -2,12 +2,13 @@ import { TestBed } from '@angular/core/testing';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { RouterTestingModule } from '@angular/router/testing';
+import { HeaderModule } from '@integrationsprojekt2/header';
 
 describe('AppComponent', () => {
     beforeEach(async () => {
         TestBed.configureTestingModule({
             declarations: [AppComponent],
-            imports: [HttpClientModule, RouterTestingModule],
+            imports: [HttpClientModule, RouterTestingModule, HeaderModule],
         }).compileComponents();
     });
 
