4e1590a6e8bb7f08bfd141ef92af535e58ddbfd8
Tim Pez <tim.pez@mni.thm.de>
2021-11-11 11:24:28 +0100

    login form styling and reactive form added

--- libs/login-form/src/lib/login-form.module.ts
+++ libs/login-form/src/lib/login-form.module.ts
@@ -1,9 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginFormComponent } from './login-form/login-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { RouterModule } from '@angular/router';
+import { LoaderModule } from '@integrationsprojekt2/loader';
 
 @NgModule({
-    imports: [CommonModule],
+    imports: [
+        CommonModule,
+        ReactiveFormsModule,
+        FontAwesomeModule,
+        RouterModule,
+        LoaderModule
+    ],
     declarations: [
       LoginFormComponent
     ],
--- libs/login-form/src/lib/login-form/login-form.component.ts
+++ libs/login-form/src/lib/login-form/login-form.component.ts
@@ -1,15 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import {
+    AbstractControl,
+    FormBuilder,
+    FormControl,
+    FormGroup,
+    Validators
+} from '@angular/forms';
 
 @Component({
   selector: 'integrationsprojekt2-login-form',
   templateUrl: './login-form.component.html',
   styleUrls: ['./login-form.component.scss']
 })
-export class LoginFormComponent implements OnInit {
+export class LoginFormComponent {
 
-  constructor() { }
+    loginForm: FormGroup;
+    loading = false;
 
-  ngOnInit(): void {
-  }
+    constructor(
+        private fb: FormBuilder
+    ) {
+        this.loginForm = this.fb.group({
+            email: new FormControl('', [
+                Validators.email,
+                Validators.required
+            ]),
+            password: new FormControl('', [
+                Validators.minLength(6),
+                Validators.required
+            ])
+        });
+    }
+
+    get email(): AbstractControl {
+        return this.loginForm.controls.email;
+    }
+
+    get password(): AbstractControl {
+        return this.loginForm.controls.password;
+    }
+
+    login(): void {
+        console.log('login with email and password');
+    }
+
+    loginWithGoogle(): void {
+        console.log('login with google');
+    }
 
 }
