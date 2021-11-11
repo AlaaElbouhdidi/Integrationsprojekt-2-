eebf8b2a28424e3fbefbbb422200b9d27f815690
Tim Pez <tim.pez@mni.thm.de>
2021-11-11 10:02:04 +0100

    login form setup added

---
+++ libs/login-form/src/lib/login-form/login-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'integrationsprojekt2-login-form',
+  templateUrl: './login-form.component.html',
+  styleUrls: ['./login-form.component.scss']
+})
+export class LoginFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- libs/login/src/lib/login.module.ts
+++ libs/login/src/lib/login.module.ts
@@ -2,9 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { LoginRoutingModule } from './login/login-routing.module';
+import { LoginFormModule } from '@integrationsprojekt2/login-form';
 
 @NgModule({
-    imports: [CommonModule, LoginRoutingModule],
+    imports: [
+        CommonModule,
+        LoginRoutingModule,
+        LoginFormModule
+    ],
     declarations: [
       LoginComponent
     ],
