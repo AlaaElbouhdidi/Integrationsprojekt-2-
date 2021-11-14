fbfe9b958237b59535d3a1d5e29f38f7c81b714c
Clemens Horn <clemens.horn@mni.thm.de>
2021-11-14 03:12:38 +0100

    migrated to new firebase project
    Signed-off-by: Clemens Horn <clemens.horn@mni.thm.de>

--- libs/env/src/lib/environment.prod.ts
+++ libs/env/src/lib/environment.prod.ts
@@ -1,17 +1,17 @@
 /**
  * Production environment variables
  */
-import { Environment } from '@integrationsprojekt2/api-interfaces'
+import { Environment } from '@integrationsprojekt2/api-interfaces';
 
 export const environment: Environment = {
     firebase: {
-        projectId: 'integrationsprojekt2',
-        appId: '1:229229352898:web:a91515cba0a9a9c03a285b',
-        storageBucket: 'integrationsprojekt2.appspot.com',
-        apiKey: 'AIzaSyCD5MCtKyB5iUyzB2H_wYLShwTp9f1H-Ks',
+        apiKey: 'AIzaSyCF0w37bc980S6avy5lVCYZqHTw4RYkGjc',
         authDomain: 'mate-team.de',
-        messagingSenderId: '229229352898',
+        projectId: 'mate-team',
+        storageBucket: 'mate-team.appspot.com',
+        messagingSenderId: '848103878945',
+        appId: '1:848103878945:web:2d78aef8264f87f52b0e60',
     },
     production: true,
-    apiUrl: 'https://europe-west1-integrationsprojekt2.cloudfunctions.net/api',
+    apiUrl: 'https://europe-west1-mate-team.cloudfunctions.net/api',
 };
