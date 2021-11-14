d0ecdd4f992aadadc8456c0aa3bdf5718d8dfced
Clemens Horn <clemens.horn@mni.thm.de>
2021-11-14 01:27:14 +0100

    update authdomain in login handler
    Signed-off-by: Clemens Horn <clemens.horn@mni.thm.de>

--- libs/env/src/lib/environment.ts
+++ libs/env/src/lib/environment.ts
@@ -9,7 +9,7 @@ export const environment: Environment = {
         appId: '1:229229352898:web:a91515cba0a9a9c03a285b',
         storageBucket: 'integrationsprojekt2.appspot.com',
         apiKey: 'AIzaSyCD5MCtKyB5iUyzB2H_wYLShwTp9f1H-Ks',
-        authDomain: 'integrationsprojekt2.firebaseapp.com',
+        authDomain: 'mate-team.de',
         messagingSenderId: '229229352898',
     },
     production: false,
