eb4175d82d6a23813a29ef6d3f9e1646ba774575
Tim Pez <tim.pez@mni.thm.de>
2021-11-17 13:21:36 +0100

    tests for email and passwort reset form added

--- libs/components/email-form/src/lib/email-form/email-form.component.spec.ts
+++ libs/components/email-form/src/lib/email-form/email-form.component.spec.ts
@@ -2,22 +2,32 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { EmailFormComponent } from './email-form.component';
 import { EmailFormModule } from '../email-form.module';
-import { environment } from '@env';
-import { AngularFireModule } from '@angular/fire/compat';
 import { RouterTestingModule } from '@angular/router/testing';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { AbstractControl, ReactiveFormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
+import { AuthService } from '@services';
 
 describe('EmailFormComponent', () => {
   let component: EmailFormComponent;
   let fixture: ComponentFixture<EmailFormComponent>;
 
+  const authServiceMock = {
+      resetPassword: jest.fn()
+  }
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+        providers: [
+            AuthService,
+            { provide: AuthService, useValue: authServiceMock }
+        ],
+
         imports: [
             EmailFormModule,
             FontAwesomeModule,
+            ReactiveFormsModule,
             RouterTestingModule.withRoutes([]),
-            AngularFireModule.initializeApp(environment.environment.firebase)
         ],
         declarations: [ EmailFormComponent ]
     })
@@ -32,5 +42,39 @@ describe('EmailFormComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should emit event on cancel', () => {
+      jest.spyOn(component.cancelResetEvent, 'emit');
+      component.cancelReset();
+      expect(component.cancelResetEvent.emit).toHaveBeenCalled();
+  });
+
+  it('should return email abstract control from form', () => {
+      expect(component.email).toBeInstanceOf(AbstractControl);
+  });
+
+  it('should render one input element', () => {
+     const form = fixture.debugElement.nativeElement.querySelector('.app-form');
+     const input = form.querySelectorAll('input');
+     expect(input.length).toEqual(1);
+  });
+
+    it('should set error when email is wrongly formatted', () => {
+        component.email.setValue('test');
+        expect(component.email.errors).not.toBeNull();
+    });
+
+  it('should trigger reset password method on form submit', () => {
+      const resetPasswordSpy = jest.spyOn(component, 'resetPassword');
+      fixture.debugElement.query(By.css('.app-form')).triggerEventHandler('ngSubmit', null);
+      fixture.detectChanges();
+      expect(resetPasswordSpy).toHaveBeenCalled();
+  });
+
+  it('should call auth service on password reset', () => {
+      const authService = jest.spyOn(authServiceMock, 'resetPassword');
+      component.resetPassword()
+      expect(authService).toHaveBeenCalled();
   });
 });
--- libs/components/password-reset-form/src/lib/password-reset-form/password-reset-form.component.spec.ts
+++ libs/components/password-reset-form/src/lib/password-reset-form/password-reset-form.component.spec.ts
@@ -2,6 +2,8 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PasswordResetFormComponent } from './password-reset-form.component';
 import { PasswordResetFormModule } from '../password-reset-form.module';
+import { AbstractControl, ReactiveFormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
 
 describe('PasswordResetFormComponent', () => {
   let component: PasswordResetFormComponent;
@@ -9,7 +11,7 @@ describe('PasswordResetFormComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-        imports: [ PasswordResetFormModule ],
+        imports: [ PasswordResetFormModule, ReactiveFormsModule ],
         declarations: [ PasswordResetFormComponent ]
     })
     .compileComponents();
@@ -24,4 +26,54 @@ describe('PasswordResetFormComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should emit event on submit', () => {
+      jest.spyOn(component.formSubmitEvent, 'emit');
+      component.resetPassword();
+      expect(component.formSubmitEvent.emit).toHaveBeenCalled();
+  });
+
+  it('should return password and email abstract controls of form', () => {
+      expect(component.newPassword).toBeInstanceOf(AbstractControl);
+      expect(component.confirmPassword).toBeInstanceOf(AbstractControl);
+  });
+
+    it('should render two input elements', () => {
+        const form = fixture.debugElement.nativeElement.querySelector('.app-form');
+        const input = form.querySelectorAll('input');
+        expect(input.length).toEqual(2);
+    });
+
+    it('should toggle state of password input on click', () => {
+        expect(component.showNewPassword).toBeFalsy();
+        fixture.debugElement.query(By.css('.toggleVisibilityIcon')).triggerEventHandler('click', null);
+        expect(component.showNewPassword).toBeTruthy();
+    });
+
+    it('should set error to null when passwords match', () => {
+        component.newPassword.setValue('testing');
+        component.confirmPassword.setValue('testing');
+        component.comparePasswords();
+        expect(component.newPassword.errors).toBeNull();
+    });
+
+    it('should set error to mustMatch when passwords do not match', () => {
+        component.newPassword.setValue('testing123');
+        component.confirmPassword.setValue('testing');
+        component.comparePasswords();
+        expect(component.confirmPassword.errors).toEqual({ "mustMatch": true });
+    });
+
+    it('should set error when password has 5 or less characters', () => {
+        component.newPassword.setValue('test');
+        component.comparePasswords();
+        expect(component.newPassword.errors).not.toBeNull();
+    });
+
+    it('should compare passwords on input event', () => {
+        const spy = jest.spyOn(component, 'comparePasswords');
+        const input = fixture.debugElement.query(By.css('input'))
+        input.triggerEventHandler('input', { target: input.nativeElement });
+        expect(spy).toHaveBeenCalled();
+    });
 });
