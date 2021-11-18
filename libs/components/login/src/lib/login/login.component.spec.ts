36477be0b7cf0f023f6d6235e2d3d016baabf29e
Tim Pez <tim.pez@mni.thm.de>
2021-11-18 10:45:42 +0100

    tests for login, alert, auth added

--- libs/components/login/src/lib/login/login.component.spec.ts
+++ libs/components/login/src/lib/login/login.component.spec.ts
@@ -2,6 +2,8 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
 import { Component } from '@angular/core';
+import { By } from '@angular/platform-browser';
+
 @Component({
     selector: 'mate-team-login-form',
     template: '<p>Mock Login Form Component</p>',
@@ -26,5 +28,10 @@ describe('LoginComponent', () => {
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('should render login form', () => {
+        expect(component.showLogin).toBeTruthy();
+        expect(fixture.debugElement.query(By.css('mate-team-login-form'))).not.toBeNull();
     });
 });
--- libs/services/src/lib/auth/auth.service.spec.ts
+++ libs/services/src/lib/auth/auth.service.spec.ts
@@ -1,32 +1,76 @@
 import { TestBed } from '@angular/core/testing';
 import { AuthService } from './auth.service';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
-import { AngularFireModule } from '@angular/fire/compat';
-
-export const environment = {
-    firebase: {
-        projectId: 'integrationsprojekt2',
-        appId: '1:229229352898:web:a91515cba0a9a9c03a285b',
-        storageBucket: 'integrationsprojekt2.appspot.com',
-        apiKey: 'AIzaSyCD5MCtKyB5iUyzB2H_wYLShwTp9f1H-Ks',
-        authDomain: 'integrationsprojekt2.firebaseapp.com',
-        messagingSenderId: '229229352898',
-    },
-    production: false,
-    apiUrl: 'http://localhost:5001/integrationsprojekt2/europe-west1/api',
-};
+import { of } from 'rxjs';
 
 describe('AuthServiceService', () => {
     let service: AuthService;
+    const email = 'email';
+    const password = 'password';
+    const code = 'code';
+
+    const angularFireAuthMock = {
+        authState: of({}),
+        signInWithEmailAndPassword: jest.fn(),
+        signInWithPopup: jest.fn(),
+        sendPasswordResetEmail: jest.fn(),
+        verifyPasswordResetCode: jest.fn(),
+        applyActionCode: jest.fn(),
+        confirmPasswordReset: jest.fn(),
+        signOut: jest.fn()
+    }
+
     beforeEach(() => {
         TestBed.configureTestingModule({
-            imports: [AngularFireModule.initializeApp(environment.firebase)],
-            providers: [AngularFireAuth],
+            providers: [
+                AngularFireAuth,
+                { provide: AngularFireAuth, useValue: angularFireAuthMock }
+            ],
         });
         service = TestBed.inject(AuthService);
     });
 
     it('should be created', () => {
         expect(service).toBeTruthy();
+    });
+
+    it('should call firebase auth signInWithEmailAndPassword in login method', () => {
+        const spy = jest.spyOn(service, 'login');
+        service.login(email, password);
+        expect(spy).toHaveBeenCalledWith(email, password);
+        expect(spy).toHaveBeenCalledTimes(1);
+    });
+
+    it('should call firebase auth signInWithPopup in loginWithGoogle method', () => {
+       const spy = jest.spyOn(service, 'loginWithGoogle');
+       service.loginWithGoogle();
+       expect(spy).toHaveBeenCalledTimes(1);
+    });
+
+    it('should call firebase auth sendPasswordResetEmail in resetPassword method', () => {
+        const spy = jest.spyOn(service, 'resetPassword');
+        service.resetPassword(email);
+        expect(spy).toHaveBeenCalledWith(email);
+        expect(spy).toHaveBeenCalledTimes(1);
+    });
+
+    it('should call firebase auth applyActionCode in verifyPasswordResetCode method', () => {
+       const spy = jest.spyOn(service, 'verifyPasswordResetCode');
+       service.verifyPasswordResetCode(code);
+       expect(spy).toHaveBeenCalledWith(code);
+       expect(spy).toHaveBeenCalledTimes(1);
+    });
+
+    it('should call firebase auth confirmPasswordReset in confirmPasswordReset method', () => {
+        const spy = jest.spyOn(service, 'confirmPasswordReset');
+        service.confirmPasswordReset(code, password);
+        expect(spy).toHaveBeenCalledWith(code, password);
+        expect(spy).toHaveBeenCalledTimes(1);
+    });
+
+    it('should call firebase auth signOut in logout method', () => {
+        const spy = jest.spyOn(service, 'logout');
+        service.logout();
+        expect(spy).toHaveBeenCalledTimes(1);
     });
 });
