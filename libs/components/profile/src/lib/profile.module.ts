02d1cb796a919cdda4a35a0eb746593226b7887d
Tim Pez <tim.pez@mni.thm.de>
2021-11-26 16:20:14 +0100

    profile page styling with example text added

--- libs/components/profile/src/lib/profile.module.ts
+++ libs/components/profile/src/lib/profile.module.ts
@@ -2,15 +2,40 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProfileComponent } from './profile/profile.component';
 import { ProfileRoutingModule } from './profile/profile-routing.module';
+import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import {
+    faUser,
+    faEnvelope,
+    faCertificate,
+    faCheckCircle,
+    faTimesCircle,
+    faLock,
+    faLink,
+    faEdit
+} from '@fortawesome/free-solid-svg-icons';
 
 @NgModule({
     imports: [
         CommonModule,
-        ProfileRoutingModule
+        ProfileRoutingModule,
+        FontAwesomeModule
     ],
     declarations: [
       ProfileComponent
     ],
     exports: [ProfileComponent]
 })
-export class ProfileModule {}
+export class ProfileModule {
+    constructor(library: FaIconLibrary) {
+        library.addIcons(
+            faUser,
+            faEnvelope,
+            faCertificate,
+            faCheckCircle,
+            faTimesCircle,
+            faLock,
+            faLink,
+            faEdit
+        );
+    }
+}
