e9908fd74c562026492d6960820ab888de335d79
Tim Pez <tim.pez@mni.thm.de>
2021-11-29 09:32:49 +0100

    profile page data, guard added, styles adjusted

--- libs/components/profile/src/lib/profile/profile-routing.module.ts
+++ libs/components/profile/src/lib/profile/profile-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProfileComponent } from './profile.component';
+import {
+    redirectUnauthorizedTo,
+    canActivate
+} from '@angular/fire/compat/auth-guard';
+
+const redirectUnauthenticatedToHome = () => redirectUnauthorizedTo(['/']);
 
 const routes: Routes = [
     {
         path: 'profile',
-        component: ProfileComponent
+        component: ProfileComponent,
+        ...canActivate(redirectUnauthenticatedToHome)
     },
 ];
 
--- libs/components/profile/src/lib/profile/profile.component.ts
+++ libs/components/profile/src/lib/profile/profile.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { AuthService } from '@services';
+import firebase from 'firebase/compat';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'mate-team-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss']
 })
-export class ProfileComponent implements OnInit {
+export class ProfileComponent implements OnInit, OnDestroy {
+    private destroy$ = new Subject();
+    user: firebase.User | null = null;
 
-  constructor() { }
+    constructor(
+        private authService: AuthService
+    ) { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+        this.authService.authState$
+            .pipe(takeUntil(this.destroy$))
+            .subscribe(user => this.user = user);
+    }
 
+    ngOnDestroy(): void {
+        this.destroy$.next();
+        this.destroy$.complete();
+    }
 }
