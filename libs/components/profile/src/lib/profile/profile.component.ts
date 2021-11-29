b2bab8f432e2277277b8f44350db3cde22fd9083
Tim Pez <tim.pez@mni.thm.de>
2021-11-29 11:41:23 +0100

    documentation added, style paths adjusted

--- libs/components/profile/src/lib/profile/profile.component.ts
+++ libs/components/profile/src/lib/profile/profile.component.ts
@@ -10,19 +10,36 @@ import { takeUntil } from 'rxjs/operators';
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit, OnDestroy {
+    /**
+     * Subject for unsubscribing from observables
+     * @private
+     */
     private destroy$ = new Subject();
+    /**
+     * The currently logged in user or null
+     */
     user: firebase.User | null = null;
 
+    /**
+     * Constructor of the profile component
+     * @param authService {AuthService}
+     */
     constructor(
         private authService: AuthService
     ) { }
 
+    /**
+     * Subscribe to auth service for currently logged in user
+     */
     ngOnInit(): void {
         this.authService.authState$
             .pipe(takeUntil(this.destroy$))
             .subscribe(user => this.user = user);
     }
 
+    /**
+     * Unsubscribe from observables
+     */
     ngOnDestroy(): void {
         this.destroy$.next();
         this.destroy$.complete();
