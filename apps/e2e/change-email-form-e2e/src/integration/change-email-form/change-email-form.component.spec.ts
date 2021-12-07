b28ef405aace3fbfeb4df28b74879800fef310b3
Clemens Horn <clemens.horn@mni.thm.de>
2021-12-07 08:55:10 +0100

    change component selector for new e2e tests
    Signed-off-by: Clemens Horn <clemens.horn@mni.thm.de>

--- apps/e2e/change-email-form-e2e/src/integration/change-email-form/change-email-form.component.spec.ts
+++ apps/e2e/change-email-form-e2e/src/integration/change-email-form/change-email-form.component.spec.ts
@@ -3,6 +3,6 @@ describe('change-email-form', () => {
         cy.visit('/iframe.html?id=changeemailformcomponent--primary')
     );
     it('should render the component', () => {
-        cy.get('integrationsprojekt2-change-email-form').should('exist');
+        cy.get('mate-team-change-email-form').should('exist');
     });
 });
--- apps/e2e/change-password-form-e2e/src/integration/change-password-form/change-password-form.component.spec.ts
+++ apps/e2e/change-password-form-e2e/src/integration/change-password-form/change-password-form.component.spec.ts
@@ -3,6 +3,6 @@ describe('change-password-form', () => {
         cy.visit('/iframe.html?id=changepasswordformcomponent--primary')
     );
     it('should render the component', () => {
-        cy.get('integrationsprojekt2-change-password-form').should('exist');
+        cy.get('mate-team-change-password-form').should('exist');
     });
 });
