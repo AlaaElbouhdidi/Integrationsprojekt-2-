5900f747c6cc69dadf4b8254ff673a1f20cdb1eb
Tim Pez <tim.pez@mni.thm.de>
2021-12-07 16:05:46 +0100

    change email, password, profile form tests added

--- libs/components/change-email-form/src/lib/change-email-form/change-email-form.component.spec.ts
+++ libs/components/change-email-form/src/lib/change-email-form/change-email-form.component.spec.ts
@@ -2,6 +2,8 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ChangeEmailFormComponent } from './change-email-form.component';
 import { ChangeEmailFormModule } from '../change-email-form.module';
+import { AbstractControl } from '@angular/forms';
+import { By } from '@angular/platform-browser';
 
 describe('ChangeEmailFormComponent', () => {
     let component: ChangeEmailFormComponent;
@@ -21,5 +23,52 @@ describe('ChangeEmailFormComponent', () => {
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('should return abstract control of password and new email', () => {
+       expect(component.password).toBeInstanceOf(AbstractControl);
+       expect(component.newEmail).toBeInstanceOf(AbstractControl);
+    });
+
+    it('should render two input elements', () => {
+       const form = fixture.debugElement.nativeElement.querySelector('form');
+       const input = form.querySelectorAll('input');
+       expect(input.length).toEqual(2);
+    });
+
+    it('should set error when password is empty', () => {
+        component.password.setValue('');
+        expect(component.password.errors).not.toBeNull();
+    });
+
+    it('should set error when email is wrongly formatted', () => {
+        component.newEmail.setValue('test');
+        expect(component.newEmail.errors).not.toBeNull();
+    });
+
+    it('should emit event on form submit', () => {
+        jest.spyOn(component.changeEmailEvent, 'emit');
+        component.changeEmail();
+        expect(component.changeEmailEvent.emit).toHaveBeenCalled();
+    });
+
+    it('should reset form on form submit', () => {
+       jest.spyOn(component.changeEmailForm, 'reset');
+       component.changeEmail();
+       expect(component.changeEmailForm.reset).toHaveBeenCalled();
+    });
+
+    it('should render loading spinner while loading and hide submit form button', () => {
+        component.loading = true;
+        fixture.detectChanges();
+        expect(fixture.debugElement.query(By.css('.loader'))).not.toBeNull();
+        expect(fixture.debugElement.query(By.css('.green-btn'))).toBeNull();
+    });
+
+    it('should render form submit button while not loading and hide loading spinner', () => {
+        component.loading = false;
+        fixture.detectChanges();
+        expect(fixture.debugElement.query(By.css('.loader'))).toBeNull();
+        expect(fixture.debugElement.query(By.css('.green-btn'))).not.toBeNull();
     });
 });
