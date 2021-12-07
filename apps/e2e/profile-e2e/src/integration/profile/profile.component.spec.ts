c3cb236eea76cd61efb3f729b3081a89bf584c23
Clemens Horn <clemens.horn@mni.thm.de>
2021-12-07 09:02:16 +0100

    change component selector for new e2e tests
    Signed-off-by: Clemens Horn <clemens.horn@mni.thm.de>

--- apps/e2e/profile-e2e/src/integration/profile/profile.component.spec.ts
+++ apps/e2e/profile-e2e/src/integration/profile/profile.component.spec.ts
@@ -1,6 +1,6 @@
 describe('profile', () => {
     beforeEach(() => cy.visit('/iframe.html?id=profilecomponent--primary'));
     it('should render the component', () => {
-        cy.get('integrationsprojekt2-profile').should('exist');
+        cy.get('mate-team-profile').should('exist');
     });
 });
