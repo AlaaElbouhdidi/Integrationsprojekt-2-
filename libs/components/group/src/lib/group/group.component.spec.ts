7869bd54b76c486c67db3abeacda7eae7dd52129
Tim Pez <tim.pez@mni.thm.de>
2021-12-09 11:38:09 +0100

    test file fixed

--- libs/components/group/src/lib/group/group.component.spec.ts
+++ libs/components/group/src/lib/group/group.component.spec.ts
@@ -1,25 +1,29 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { GroupComponent } from './group.component';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('GroupComponent', () => {
-  let component: GroupComponent;
-  let fixture: ComponentFixture<GroupComponent>;
+    let component: GroupComponent;
+    let fixture: ComponentFixture<GroupComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ GroupComponent ]
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [
+                RouterTestingModule.withRoutes([]),
+            ],
+            declarations: [ GroupComponent ]
     })
     .compileComponents();
-  });
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(GroupComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(GroupComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
