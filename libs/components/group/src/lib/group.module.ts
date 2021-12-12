4458d3a8b4e20611e31701ce444bf15f69faf4a1
Tim Pez <tim.pez@mni.thm.de>
2021-12-12 10:49:10 +0100

    group navigation setup added for child routes

--- libs/components/group/src/lib/group.module.ts
+++ libs/components/group/src/lib/group.module.ts
@@ -2,10 +2,30 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GroupComponent } from './group/group.component';
 import { GroupRoutingModule } from './group/group-routing.module';
+import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import {
+    faCommentAlt,
+    faUserFriends,
+    faTable,
+    faCalendarAlt
+} from '@fortawesome/free-solid-svg-icons';
 
 @NgModule({
-    imports: [CommonModule, GroupRoutingModule],
+    imports: [
+        CommonModule,
+        GroupRoutingModule,
+        FontAwesomeModule
+    ],
     declarations: [GroupComponent],
     exports: [GroupComponent]
 })
-export class GroupModule {}
+export class GroupModule {
+    constructor(library: FaIconLibrary) {
+        library.addIcons(
+            faCommentAlt,
+            faUserFriends,
+            faTable,
+            faCalendarAlt
+        );
+    }
+}
--- libs/components/group/src/lib/group/group.component.ts
+++ libs/components/group/src/lib/group/group.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 import { Subject, takeUntil } from 'rxjs';
 
 @Component({
@@ -10,8 +10,14 @@ import { Subject, takeUntil } from 'rxjs';
 export class GroupComponent implements OnInit, OnDestroy {
     private destroy$ = new Subject();
     groupId: string | null = null;
+    activeRoute = '';
 
-    constructor(private route: ActivatedRoute) {}
+    constructor(
+        private route: ActivatedRoute,
+        private router: Router
+    ) {
+        this.activeRoute = this.router.url;
+    }
 
     ngOnInit(): void {
         this.route.paramMap
@@ -19,6 +25,17 @@ export class GroupComponent implements OnInit, OnDestroy {
             .subscribe((param) => {
                 this.groupId = param.get('id');
             });
+        this.router.events
+            .pipe(takeUntil(this.destroy$))
+            .subscribe((event) => {
+                if (event instanceof NavigationEnd) {
+                    this.activeRoute = this.router.url;
+                }
+            });
+    }
+
+    navigateTo(routeParam: 'events' | 'statistics' | 'chat' | 'members') {
+        this.router.navigate([routeParam], { relativeTo: this.route });
     }
 
     ngOnDestroy(): void {
