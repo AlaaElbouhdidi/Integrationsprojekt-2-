ae4a284578eac99bb1152c2621a35566d99e2726
Tim Pez <tim.pez@mni.thm.de>
2021-12-20 15:31:33 +0100

    tests added for group-polls-events component

--- libs/components/group-polls-events/src/lib/group-polls-events/group-polls-events.component.spec.ts
+++ libs/components/group-polls-events/src/lib/group-polls-events/group-polls-events.component.spec.ts
@@ -1,25 +1,130 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { GroupPollsEventsComponent } from './group-polls-events.component';
+import { AlertService, AuthService, GroupService, PollService } from '@services';
+import { GroupPollsEventsModule } from '../group-polls-events.module';
+import { Poll }  from '@api-interfaces';
 
 describe('GroupPollsEventsComponent', () => {
-  let component: GroupPollsEventsComponent;
-  let fixture: ComponentFixture<GroupPollsEventsComponent>;
+    let component: GroupPollsEventsComponent;
+    let fixture: ComponentFixture<GroupPollsEventsComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ GroupPollsEventsComponent ]
+    const pollServiceMock = {
+        getPolls: jest.fn(),
+        createPoll: jest.fn(),
+        updatePoll: jest.fn(),
+        deletePoll: jest.fn()
+    };
+    const authServiceMock = {
+        getCurrentUser: jest.fn().mockReturnValue({ uid: 'userId' })
+    };
+    const groupServiceMock = {
+        getGroupById: jest.fn()
+    };
+    const alertServiceMock = {
+        addAlert: jest.fn()
+    };
+    const pollMock: Poll = {
+        id: 'pollId', title: 'poll', choices: [{ date: '01/01/2021', votes: 2 }], usersVoted: []
+    };
+
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            providers: [
+                { provide: PollService, useValue: pollServiceMock },
+                { provide: AuthService, useValue: authServiceMock },
+                { provide: GroupService, useValue: groupServiceMock },
+                { provide: AlertService, useValue: alertServiceMock }
+            ],
+            imports: [GroupPollsEventsModule],
     })
     .compileComponents();
-  });
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(GroupPollsEventsComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(GroupPollsEventsComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('should render info message if no poll active', () => {
+        const message = fixture.debugElement.nativeElement.querySelector('.polls-body-message');
+        expect(component.polls.length).toEqual(0);
+        expect(message).not.toBeNull();
+    });
+
+    it('should return false on admin check if user is not admin', () => {
+        const spy = jest.spyOn(component, 'checkIfAdmin');
+        component.checkIfAdmin('adminId');
+        expect(spy).toReturnWith(false);
+    });
+
+    it('should return true on admin check if user is admin', () => {
+        const spy = jest.spyOn(component, 'checkIfAdmin');
+        component.checkIfAdmin('userId');
+        expect(spy).toReturnWith(true);
+    });
+
+    it('should not create poll if user is not admin of the group', () => {
+        component.group.admin = 'adminId';
+        fixture.detectChanges();
+        const spy = jest.spyOn(pollServiceMock, 'createPoll');
+        component.createPoll(pollMock);
+        expect(spy).not.toHaveBeenCalled();
+    });
+
+    it('should create poll if user is admin of the group', () => {
+        component.group.admin = 'userId';
+        fixture.detectChanges();
+        const spy = jest.spyOn(pollServiceMock, 'createPoll');
+        const alertSpy = jest.spyOn(alertServiceMock, 'addAlert');
+        component.createPoll(pollMock);
+        expect(spy).toHaveBeenCalled();
+        expect(alertSpy).toHaveBeenCalled();
+    });
+
+    it('should not update poll if user has already voted', () => {
+        const pollMockWithUserVote = {
+            title: 'poll',
+            choices: [
+                { date: '01/01/2021', votes: 2 }
+            ],
+            usersVoted: ['userId']
+        };
+        const spy = jest.spyOn(pollServiceMock, 'updatePoll');
+        component.updatePoll(pollMockWithUserVote);
+        expect(spy).not.toHaveBeenCalled();
+    });
+
+    it('should update poll if user has not voted yet', () => {
+        const spy = jest.spyOn(pollServiceMock, 'updatePoll');
+        const alertSpy = jest.spyOn(alertServiceMock, 'addAlert');
+        component.updatePoll(pollMock);
+        expect(spy).toHaveBeenCalled();
+        expect(alertSpy).toHaveBeenCalled();
+    });
+
+    it('should get group on component init', () => {
+        const spy = jest.spyOn(groupServiceMock, 'getGroupById');
+        component.ngOnInit();
+        expect(spy).toHaveBeenCalled();
+    });
+
+    it('should get polls on component init', () => {
+        const spy = jest.spyOn(pollServiceMock, 'getPolls');
+        component.ngOnInit();
+        expect(spy).toHaveBeenCalled();
+    });
+
+    it('should trigger unsubscribe from observables on destroy', () => {
+        jest.spyOn(component['destroy$'], 'next');
+        jest.spyOn(component['destroy$'], 'complete');
+        component.ngOnDestroy();
+        expect(component['destroy$'].next).toHaveBeenCalled();
+        expect(component['destroy$'].complete).toHaveBeenCalled();
+    });
 });
