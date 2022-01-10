207c1385b8eadea20b30319f77ac718b7e2dda33
Clemens Horn <clemens.horn@mni.thm.de>
2022-01-10 13:12:24 +0100

    fetch groups from firestore
    Signed-off-by: Clemens Horn <clemens.horn@mni.thm.de>

--- libs/components/groups/src/lib/groups.module.ts
+++ libs/components/groups/src/lib/groups.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GroupsComponent } from './groups/groups.component';
+import { ServicesModule } from '@services';
 
 @NgModule({
-    imports: [CommonModule],
-    declarations: [
-      GroupsComponent
-    ]
+    imports: [CommonModule, ServicesModule],
+    declarations: [GroupsComponent],
+    exports: [GroupsComponent]
 })
 export class GroupsModule {}
--- libs/components/groups/src/lib/groups/groups.component.ts
+++ libs/components/groups/src/lib/groups/groups.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
+import { Group } from '@api-interfaces';
+import { GroupService } from '@services';
+import { Observable } from 'rxjs';
 
 @Component({
     selector: 'mate-team-groups',
     templateUrl: './groups.component.html',
     styleUrls: ['./groups.component.scss']
 })
-export class GroupsComponent {}
+export class GroupsComponent {
+    groups!: Observable<Group[]>
+    constructor(public groupService: GroupService) {
+        this.groups = this.groupService.groupCollection.valueChanges().pipe();
+    }
+}
