09cefc7059a554095bbb770f60bc23ce1a170ac2
Tim Pez <tim.pez@mni.thm.de>
2022-01-12 15:00:21 +0100

    test files errors fixed for pipeline

--- libs/components/edit-event-form/src/lib/edit-event-form/edit-event-form.component.spec.ts
+++ libs/components/edit-event-form/src/lib/edit-event-form/edit-event-form.component.spec.ts
@@ -1,25 +1,25 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { EditEventFormComponent } from './edit-event-form.component';
+import { EditEventFormModule } from '../edit-event-form.module';
 
 describe('EditEventFormComponent', () => {
-  let component: EditEventFormComponent;
-  let fixture: ComponentFixture<EditEventFormComponent>;
+    let component: EditEventFormComponent;
+    let fixture: ComponentFixture<EditEventFormComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ EditEventFormComponent ]
-    })
-    .compileComponents();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [EditEventFormModule]
+        }).compileComponents();
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(EditEventFormComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(EditEventFormComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
--- libs/components/edit-event-modal/src/lib/edit-event-modal/edit-event-modal.component.spec.ts
+++ libs/components/edit-event-modal/src/lib/edit-event-modal/edit-event-modal.component.spec.ts
@@ -1,25 +1,25 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { EditEventModalComponent } from './edit-event-modal.component';
+import { EditEventModalModule } from '../edit-event-modal.module';
 
 describe('EditEventModalComponent', () => {
-  let component: EditEventModalComponent;
-  let fixture: ComponentFixture<EditEventModalComponent>;
+    let component: EditEventModalComponent;
+    let fixture: ComponentFixture<EditEventModalComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ EditEventModalComponent ]
-    })
-    .compileComponents();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [EditEventModalModule]
+        }).compileComponents();
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(EditEventModalComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(EditEventModalComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
